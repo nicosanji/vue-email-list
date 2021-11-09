ee0e7dd6844b4ac1eb75d29f6261091ea9dc1b2f
Nicolò Sangiorgio <n.sangiorgio@hotmail.it>
2021-11-09 16:23:08 +0100

    DEF. 1.0: genera 10 mail random tramite API

--- script.js
+++ script.js
@@ -8,25 +8,27 @@
 Vue.config.devtools = true;
 
 new Vue({
-
     el: "#root",
-
     data: {
-        titleH1: "Vue email List",
-        esempioMailList: [
-            { mail: 'gigi@gmail.com' },
-            { mail: 'anna@gmail.com' },
-            { mail: 'mario@gmail.com' },
-            { mail: 'luisa@gmail.com' },
-            { mail: 'alberto@gmail.com' },
-        ],
+        titleH1: "Vue Email List",
+        btnText: "Generate 10 Mail",
+        // ritorna una email {"success":true,"response":"clay21@yahoo.com"}
+        urlApiMail: "https://flynn.boolean.careers/exercises/api/random/mail",
+        mailList: [],
     },
-
     methods: {
-
-    },
-
-    mounted() {
-
-    },
+        // genera una lista di 10 email random tramite API @click btn
+        getMailList() {
+            this.mailList = [];
+            for (let i = 0; i < 10; i++) {
+                // .get -> richiesta al server .then -> risposta del server
+                axios.get(this.urlApiMail).then((ajaxResponse) => {
+                    
+                    const rispostaServer = ajaxResponse.data;
+                    // stampo i dati ricevuti dal server
+                    this.mailList.push(rispostaServer.response);
+                })
+            }
+        },
+    }
 });
